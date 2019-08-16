50f78c96ccba70fd168c6c48889cefc1ef119fff
Sascha Lemke <sascha-lemke@gmx.de>
2019-08-16 15:47:49 +0200

    added accordion without foundation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,36 @@
-const mainMenu = document.getElementById('icon-main-menu');
+document.addEventListener('DOMContentLoaded', () => {
 
-const openSidebar = (event) => {
-    const sidebar = document.getElementById('offCanvas');
-    const content = document.querySelectorAll('#offCanvas + .off-canvas-content')[0];
+    const mainMenu = document.getElementById('icon-main-menu');
 
-    if(sidebar) {
-        sidebar.classList.toggle('is-open');
+    const openSidebar = (event) => {
+        const sidebar = document.getElementById('offCanvas');
+        const content = document.querySelectorAll('#offCanvas + .off-canvas-content')[0];
+    
+        if(sidebar) {
+            sidebar.classList.toggle('is-open');
+        }
+    
+        if(content) {
+            content.classList.toggle('is-open');
+        }
+    };
+    
+    mainMenu.addEventListener('click', openSidebar);
+    
+    
+    const accordionTitles = document.querySelectorAll('.accordion-title');
+    
+    const toggleAccordion = (event) => {
+        event.preventDefault();
+
+        const title = event.currentTarget;
+        title.classList.toggle('active');
+
+        const content = title.nextElementSibling;
+        content.classList.toggle('active');
+    };
+    
+    for(let title of accordionTitles) {
+        title.addEventListener('click', toggleAccordion);
     }
-
-    if(content) {
-        content.classList.toggle('is-open');
-    }
-};
-
-mainMenu.addEventListener('click', openSidebar);
+});
